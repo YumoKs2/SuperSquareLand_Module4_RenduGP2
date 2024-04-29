b8eec0887e16aa369111c4326d8a2d20adaf2d92
YumoKs2 <146093935+YumoKs2@users.noreply.github.com>
2024-04-29 11:30:10 +0200

    third commit
    correction on code and adding acceleration

--- Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
@@ -26,14 +26,12 @@ public class HeroController : MonoBehaviour
     private float GetInputMoveX()
     {
         float inputMoveX = 0f;
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Q))
-        {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Q)) {
             //Negative means : To the left <=
-            inputMoveX = 1f;
+            inputMoveX = -1f;
         }
 
-        if (Input.GetKey(KeyCode.D))
-        {
+        if (Input.GetKey(KeyCode.D)) {
             //Positive means : To the right =>
             inputMoveX = 1f;
         }
--- Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
@@ -6,7 +6,8 @@ public class HeroEntity : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidbody;
 
     [Header("Horizontal Movements")]
-    [SerializeField] private float _horizontalSpeed = 5f;
+    [SerializeField]private HeroHorizontalMovementsSettings _movementsSettings;
+    private float _horizontalSpeed = 0f;
     private float _moveDirX = 0f;
 
     [Header("Orientation")]
@@ -23,14 +24,14 @@ public class HeroEntity : MonoBehaviour
 
     private void FixedUpdate()
     {
+        _UpdateHorizontalSpeed();
         _ChangeOrientFromHorizontalMovement();
         _ApplyHorizontalSpeed();
-
     }
 
     private void _ChangeOrientFromHorizontalMovement()
-        {
-            if (_moveDirX == 0f) return;
+    {
+        if (_moveDirX == 0f) return;
         _orientX = Mathf.Sign(_moveDirX);
     }
 
@@ -63,5 +64,22 @@ public class HeroEntity : MonoBehaviour
         GUILayout.Label($"OrientX = {_orientX}");
         GUILayout.Label($"Horizontal Speed = {_horizontalSpeed}");
         GUILayout.EndVertical();
+    }
+
+    private void _Accelerate()
+    {
+        _horizontalSpeed += _movementsSettings.acceleration * Time.fixedDeltaTime;
+        if (_horizontalSpeed > _movementsSettings.speedMax ) {
+            _horizontalSpeed = _movementsSettings.speedMax;
+        }
+    }
+
+    private void _UpdateHorizontalSpeed()
+    {
+        if (_moveDirX != 0f) {
+            _Accelerate();
+        } else {
+            _horizontalSpeed = 0f;
+        }
     }
 }
