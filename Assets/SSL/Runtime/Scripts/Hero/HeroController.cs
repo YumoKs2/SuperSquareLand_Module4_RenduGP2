0d10e59ca4a75a7c3be493031965f394b2ecee34
YumoKs2 <146093935+YumoKs2@users.noreply.github.com>
2024-04-30 12:07:26 +0200

    tenth commit
    adding controller duration and buffer duration

--- Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
@@ -6,6 +6,10 @@ public class HeroController : MonoBehaviour
     [Header("Entity")]
     [SerializeField] private HeroEntity _entity;
 
+    [Header("Jump Buffer")]
+    [SerializeField] private float _jumpBufferDuration = 0.2f;
+    private float _jumpBufferTimer = 0f;
+
     [Header("Debug")]
     [SerializeField] private bool _guiDebug = false;
 
@@ -15,22 +19,39 @@ public class HeroController : MonoBehaviour
 
         GUILayout.BeginVertical(GUI.skin.box);
         GUILayout.Label(gameObject.name);
+        GUILayout.Label($"Jump Buffer Timer = {_jumpBufferTimer}");
         GUILayout.EndVertical();
     }
 
     private void Update()
     {
+        _UpdateJumpBuffer();
+
         _entity.SetMoveDirX(GetInputMoveX());
 
-        if (_GetInputDownJump()) {
-            if (_entity.IsTouchingGround && !_entity.IsJumping) {
+        if (_GetInputDownJump())
+        {
+            if (_entity.IsTouchingGround && !_entity.IsJumping)
+            {
+                _entity.JumpStart();
+            } else {
+                _ResetJumpBuffer();
+            }
+        }
+
+        if (IsJumpBufferActive()) {
+            if (_entity.IsTouchingGround && _entity.IsJumping)
+            {
                 _entity.JumpStart();
             }
         }
 
-        if (_entity.IsJumpingImpulsing) {
-            if(!_GetInputDownJump() && _entity.IsJumpingMinDurationReached) { 
-            _entity.StopJumpImpulsion();}
+        if (_entity.IsJumpingImpulsing)
+        {
+            if (!_GetInputDownJump() && _entity.IsJumpingMinDurationReached)
+            {
+                _entity.StopJumpImpulsion();
+            }
         }
 
         /*code pour le dash (ne fonctionne pas)
@@ -75,5 +96,31 @@ public class HeroController : MonoBehaviour
     private bool _GetInputJump()
     {
         return Input.GetKey(KeyCode.Space);
+    }
+
+    private void _ResetJumpBuffer()
+    {
+        _jumpBufferTimer = 0f;
+    }
+
+    private bool IsJumpBufferActive()
+    {
+        return _jumpBufferTimer < _jumpBufferDuration;
+    }
+
+    private void _UpdateJumpBuffer()
+    {
+        if (!IsJumpBufferActive()) return;
+        _jumpBufferTimer += Time.deltaTime;
+    }
+
+    private void _CancelJumpBuffer()
+    {
+        _jumpBufferTimer = _jumpBufferDuration;
+    }
+
+    private void Start()
+    {
+        _CancelJumpBuffer();
     }
 }
