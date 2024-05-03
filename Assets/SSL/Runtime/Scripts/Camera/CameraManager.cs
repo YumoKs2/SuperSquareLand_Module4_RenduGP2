04951bb0d794ccf14bafaf0f41f70698912660e6
YumoKs2 <146093935+YumoKs2@users.noreply.github.com>
2024-05-03 10:03:30 +0200

    thirteenth commit
    adding camera trigger

--- Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
@@ -2,10 +2,63 @@
 
 public class CameraManager : MonoBehaviour
 {
+    public static CameraManager Instance { get; private set; }
+
     [Header("Camera")]
     [SerializeField] private Camera _camera;
 
     [Header("Profile System")]
     [SerializeField] private CameraProfile _defaultCameraProfile;
     private CameraProfile _currentCameraProfile;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        _InitToDefaultProfile();
+    }
+
+    private void Update()
+    {
+        _SetCameraPosition(_currentCameraProfile.Position);
+        _SetCameraSize(_currentCameraProfile.CameraSize);
+    }
+
+    //pour changer la position de notre caméra en x et y
+    private void _SetCameraPosition(Vector3 position)
+    {
+        Vector3 newCameraPosition = _camera.transform.position;
+        newCameraPosition.x = position.x;
+        newCameraPosition.y = position.y;
+        _camera.transform.position = newCameraPosition;
+    }
+
+
+    //pour changer la taille de la caméra
+    private void _SetCameraSize(float size)
+    {
+        _camera.orthographicSize = size;
+    }
+
+    private void _InitToDefaultProfile()
+    {
+        _currentCameraProfile = _defaultCameraProfile;
+        _SetCameraPosition(_currentCameraProfile.Position);
+        _SetCameraSize(_currentCameraProfile.CameraSize);
+    }
+
+    public void EnterProfile(CameraProfile cameraProfile)
+    {
+        _currentCameraProfile = cameraProfile;
+    }
+
+    public void ExitProfile(CameraProfile cameraProfile)
+    {
+        if (_currentCameraProfile != cameraProfile) return;
+        _currentCameraProfile = _defaultCameraProfile;
+    }
 }
--- Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
@@ -2,5 +2,17 @@
 
 public class CameraProfile : MonoBehaviour
 {
-}
+    private Camera _camera;
 
+    public float CameraSize => _camera.orthographicSize;
+
+    public Vector3 Position => _camera.transform.position;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+        if ( _camera != null) {
+            _camera.enabled = false;
+        }
+    }
+}
