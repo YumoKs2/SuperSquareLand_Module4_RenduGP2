274591d839e547f5bf3dda72686421ae6d6d1dc6
YumoKs2 <146093935+YumoKs2@users.noreply.github.com>
2024-05-03 12:42:11 +0200

    eighteenth commit
    adding camera bounds and rect

--- Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
@@ -34,6 +34,7 @@ public class CameraManager : MonoBehaviour
     private void Update()
     {
         Vector3 nextPosition = _FindCameraNextPosition();
+        nextPosition = _ClampPositionIntoBounds(nextPosition);
         nextPosition =_ApplyDamping(nextPosition);
 
         if (_IsPlayingProfileTransition()) {
@@ -69,7 +70,7 @@ public class CameraManager : MonoBehaviour
         _currentCameraProfile = _defaultCameraProfile;
         _SetCameraPosition(_currentCameraProfile.Position);
         _SetCameraSize(_currentCameraProfile.CameraSize);
-        _SetCameraDampedPosition(_FindCameraNextPosition());
+        _SetCameraDampedPosition(_ClampPositionIntoBounds(_FindCameraNextPosition()));
     }
 
     public void EnterProfile(CameraProfile cameraProfile, CameraProfileTransition transition = null)
@@ -162,5 +163,31 @@ public class CameraManager : MonoBehaviour
     {
         _dampedPosition.x = position.x;
         _dampedPosition.y = position.y;
+    }
+
+    private Vector3 _ClampPositionIntoBounds(Vector3 position)
+    {
+        if (!_currentCameraProfile.HasBounds) return position;
+
+        Rect boundsRect = _currentCameraProfile.BoundsRect;
+        Vector3 worldBottomLeft = _camera.ScreenToWorldPoint(new Vector3(0f, 0f));
+        Vector3 worldTopRight = _camera.ScreenToWorldPoint(new Vector3(_camera.pixelWidth, _camera.pixelHeight));
+        Vector2 worldScreenSize = new Vector2(worldTopRight.x - worldBottomLeft.x, worldTopRight.y - worldBottomLeft.y);
+        Vector2 worldHalfScreenSize = worldScreenSize / 2f;
+
+        if (position.x > boundsRect.xMax - worldHalfScreenSize.x) {
+            position.x = boundsRect.xMax - worldHalfScreenSize.x;
+        }
+        if (position.x < boundsRect.xMin + worldHalfScreenSize.x) {
+            position.x = boundsRect.xMin + worldHalfScreenSize.x;
+        }
+        if (position.y > boundsRect.xMax - worldHalfScreenSize.y) {
+            position.y = boundsRect.yMax - worldHalfScreenSize.y;
+        }
+        if (position.y < boundsRect.yMin + worldHalfScreenSize.y) {
+            position.y = boundsRect.yMin + worldHalfScreenSize.y;
+        }
+
+        return position;
     }
 }
--- Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
@@ -15,6 +15,10 @@ public class CameraProfile : MonoBehaviour
     [SerializeField] private bool _useDampingVertically = false;
     [SerializeField] private float _verticalDampingFactor = 5f;
 
+    [Header("Bounds")]
+    [SerializeField] private bool _hasBounds = false;
+    [SerializeField] private Rect _boundsRect = new Rect(0f, 0f, 10f, 10f);
+
     public float CameraSize => _camera.orthographicSize;
 
     public Vector3 Position => _camera.transform.position;
@@ -44,4 +48,15 @@ public class CameraProfile : MonoBehaviour
     public bool UseDampingVertically => _useDampingVertically;
    
     public float VerticalDampingFactor => _verticalDampingFactor;
+
+    public bool HasBounds => _hasBounds;
+
+    public Rect BoundsRect => _boundsRect;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!_hasBounds) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(_boundsRect.center, _boundsRect.size);
+    }
 }
